0e73394747a509016e02e2de168ba96bc545ffe4
aldosch <git@aldo.io>
2024-06-27 13:16:48 +1000

    add maxDuration 900

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
+export const maxDuration = 900;
 
 export async function GET(request: NextRequest) {
   try {
